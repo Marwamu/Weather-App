ff6925535ff6d6a5812b7516dcae037bc8a4dd75
Marwa Mohamed <111978329+Marwamu@users.noreply.github.com>
2024-08-14 16:11:00 +0300

    fix api url

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const apiKey = "387475101415870779f16fdfc6e13277";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?unites=metrics&q=bangalore";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?unites=metrics&q=berlin";
 
 async function checkWeather(){
-    const response = await fetch( apiURL + `&appid =${apiKey}`)
+    const response = await fetch( apiURL + `&appid=${apiKey}`)
     var data = await response.json();
+    console.log(data);
 }
+
+checkWeather();
