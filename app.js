c864c49db6ecbd230fc3d38aecea95558f59f755
Marwa Mohamed <111978329+Marwamu@users.noreply.github.com>
2024-08-14 14:49:08 +0300

    Design card

---
+++ app.js
@@ -0,0 +1,7 @@
+const apiKey = "387475101415870779f16fdfc6e13277";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?unites=metrics&q=bangalore";
+
+async function checkWeather(){
+    const response = await fetch( apiURL + `&appid =${apiKey}`)
+    var data = await response.json();
+}
